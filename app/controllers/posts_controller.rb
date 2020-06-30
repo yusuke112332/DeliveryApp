b554ebbbd48960bf7b426fd9decfe7f05c7c6842
yusuke112332 <yusuke123491@gmail.com>
2020-06-30 19:01:37 +0900

    投稿の削除機能と詳細機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,20 @@ class PostsController < ApplicationController
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
   end
 
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:food_name, :caption, photos_attributes: [:image]).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(new create index) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
 end
