42ea60f57d8c46a19aa6308a0a438446f3964368
yusuke112332 <yusuke123491@gmail.com>
2020-07-02 18:28:08 +0900

    ユーザー一覧画面作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  mount_uploader :image, ImageUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
